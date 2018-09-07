e25803a0785b71a97502962dd8a2cbed69e4ba07
Avi-keshri <kravinash321@gmail.com>
2018-09-07 14:12:36 +0530

    Initial commit

---
+++ This/index.php
@@ -0,0 +1,20 @@
+<!-- This keyword works as object refrence and
+<?php
+
+class Test{
+	 public $a;
+	 public $b;
+	public function __construct($a ,$b)
+	{
+		$this->a=$a;
+		$this->b=$b;
+		echo "<pre>";
+		print_r($this);
+	}
+}
+
+$obj =new Test(1,2);
+$obj1 =new Test(2,4);
+
+
+
